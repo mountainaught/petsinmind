82e0d1a48938350b9471af1cb52d240a4351df28
Dimitrios <dimppnm@gmail.com>
2025-04-06 12:56:33 +0100

    initial fuctions commit

--- backend/src/main/java/com/petsinmind/Appointment.java
+++ backend/src/main/java/com/petsinmind/Appointment.java
@@ -1,5 +1,118 @@
 package com.petsinmind;
 
+import java.util.Calendar;
+import java.util.List;
 // Dimitris
+import com.petsinmind.Review;
+import com.petsinmind.Pet;
+import com.petsinmind.users.Caretaker;
+import com.petsinmind.users.PetOwner;
+
 public class Appointment {
+    private Caretaker caretaker;
+    private PetOwner PetOwner;
+    private List<Pet> pets;
+    private Calendar startDate;
+    private Calendar endDate;
+    private String Type;
+
+    // Initial constructor
+    public Appointment() {
+        this.caretaker = null;
+        this.PetOwner = null;
+        this.pets = null;
+        this.startDate = null;
+        this.endDate = null;
+        this.Type = null;
+    }
+
+    // Constructor with parameters
+    public Appointment(Caretaker caretaker, PetOwner PetOwner, List<Pet> pets, Calendar startDate, Calendar endDate,
+            String Type) {
+        this.caretaker = caretaker;
+        this.PetOwner = PetOwner;
+        this.pets = pets;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.Type = Type;
+    }
+
+    // Getters and Setters
+    public Caretaker getCaretaker() {
+        return caretaker;
+    }
+
+    public void setCaretaker(Caretaker caretaker) {
+        this.caretaker = caretaker;
+    }
+
+    public PetOwner getPetOwner() {
+        return PetOwner;
+    }
+
+    public void setPetOwner(PetOwner PetOwner) {
+        this.PetOwner = PetOwner;
+    }
+
+    public List<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(List<Pet> pets) {
+        this.pets = pets;
+    }
+
+    public void addPet(Pet pet) {
+        this.pets.add(pet);
+    }
+
+    public void removePet(Pet pet) {
+        this.pets.remove(pet);
+    }
+
+    public Calendar getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Calendar startDate) {
+        this.startDate = startDate;
+    }
+
+    public Calendar getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Calendar endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(String Type) {
+        this.Type = Type;
+    }
+
+    public Review Review(String Details, int Rating) {
+        return new Review(Details, Rating, this, this.PetOwner, this.caretaker);
+    }
+
+    public Payment MakePayment(List<String> SenderInfo, List<String> ReceiverInfo, PetOwner PetOwner,
+            Caretaker caretaker) {
+        return new Payment("123123123", "Credit Card", Calendar.getInstance(), 100.0, "USD",
+                PetOwner.getUserID(), caretaker.getUserID());
+    }
+
+    // toString method
+    public String toString() {
+        return "Appointment{" +
+                "caretaker=" + caretaker +
+                ", PetOwner=" + PetOwner +
+                ", pets=" + pets +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", Type='" + Type + '\'' +
+                '}';
+    }
 }
--- backend/src/main/java/com/petsinmind/Payment.java
+++ backend/src/main/java/com/petsinmind/Payment.java
@@ -1,5 +1,107 @@
 package com.petsinmind;
 
+import java.util.Calendar;
+
 // Dimitris
 public class Payment {
+    private String PaymentID;
+    private String PaymentMethod;
+    private Calendar PaymentDate;
+    private float PaymentAmount;
+    private String PaymentCurrency;
+    private int SenderID;
+    private int ReceiverID;
+
+    // Initial constructor
+    public Payment() {
+        this.PaymentID = null;
+        this.PaymentMethod = null;
+        this.PaymentDate = null;
+        this.PaymentAmount = 0.0f;
+        this.PaymentCurrency = null;
+        this.SenderID = 0;
+        this.ReceiverID = 0;
+    }
+
+    // Constructor with parameters
+    public Payment(String PaymentID, String PaymentMethod, Calendar PaymentDate, float PaymentAmount,
+            String PaymentCurrency, int SenderID,
+            int ReceiverID) {
+        this.PaymentID = PaymentID;
+        this.PaymentMethod = PaymentMethod;
+        this.PaymentDate = PaymentDate;
+        this.PaymentAmount = PaymentAmount;
+        this.SenderID = SenderID;
+        this.ReceiverID = ReceiverID;
+    }
+
+    // Getters and Setters
+    public String getPaymentID() {
+        return PaymentID;
+    }
+
+    public void setPaymentID(String PaymentID) {
+        this.PaymentID = PaymentID;
+    }
+
+    public String getPaymentMethod() {
+        return PaymentMethod;
+    }
+
+    public void setPaymentMethod(String PaymentMethod) {
+        this.PaymentMethod = PaymentMethod;
+    }
+
+    public Calendar getPaymentDate() {
+        return PaymentDate;
+    }
+
+    public void setPaymentDate(Calendar PaymentDate) {
+        this.PaymentDate = PaymentDate;
+    }
+
+    public float getPaymentAmount() {
+        return PaymentAmount;
+    }
+
+    public void setPaymentAmount(float PaymentAmount) {
+        this.PaymentAmount = PaymentAmount;
+    }
+
+    public String getPaymentCurrency() {
+        return PaymentCurrency;
+    }
+
+    public void setPaymentCurrency(String PaymentCurrency) {
+        this.PaymentCurrency = PaymentCurrency;
+    }
+
+    public int getSenderID() {
+        return SenderID;
+    }
+
+    public void setSenderID(int SenderID) {
+        this.SenderID = SenderID;
+    }
+
+    public int getReceiverID() {
+        return ReceiverID;
+    }
+
+    public void setReceiverID(int ReceiverID) {
+        this.ReceiverID = ReceiverID;
+    }
+    // End of getters and setters
+
+    public String toString() {
+        return "Payment{" +
+                "PaymentID='" + PaymentID + '\'' +
+                ", PaymentMethod='" + PaymentMethod + '\'' +
+                ", PaymentDate='" + PaymentDate + '\'' +
+                ", PaymentAmount='" + PaymentAmount + '\'' +
+                ", PaymentCurrency='" + PaymentCurrency + '\'' +
+                ", SenderID='" + SenderID + '\'' +
+                ", ReceiverID='" + ReceiverID + '\'' +
+                '}';
+    }
 }
