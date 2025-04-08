5b45d730f9ee5f0cf2a501483661a63b199283d4
ZioFalak <falak16sm@gmail.com>
2025-04-09 00:00:35 +0100

    changing attributes in users

--- backend/src/main/java/com/petsinmind/users/User.java
+++ backend/src/main/java/com/petsinmind/users/User.java
@@ -2,6 +2,7 @@ package com.petsinmind.users;
 
 import com.petsinmind.messages.Message;
 
+import java.rmi.registry.Registry;
 import java.util.*;
 
 // Tibet
@@ -100,11 +101,69 @@ public abstract class User {
 	}
 
 	/**
-	 * 
+	 * Generic method to change a user attribute and update the registry.
+	 *
 	 * @param Message
 	 */
 	// public boolean SendMessage(Message Message) {
 
 	// }
 
+	private boolean changeAttribute(String currentValue, String newValue, String attribute, Registry registry) {
+		if (newValue == null || newValue.isEmpty() || newValue.equals(currentValue)) {
+			return false;
+		}
+
+		// Update the attribute value
+		switch (attribute) {
+			case "FirstName":
+				this.FirstName = newValue;
+				break;
+			case "LastName":
+				this.LastName = newValue;
+				break;
+			case "UserName":
+				this.UserName = newValue;
+				break;
+			case "UserPassword":
+				this.UserPassword = newValue;
+				break;
+			case "UserEmail":
+				this.UserEmail = newValue;
+				break;
+			case "PhoneNumber":
+				this.PhoneNumber = newValue;
+				break;
+			default:
+				return false; // Invalid attribute
+		}
+	
+		// Update the user in the registry
+		return registry.editUser(this);
+	}
+
+	// Refactored methods
+	public boolean changeFirstName(String newFirstName, Registry registry) {
+		return changeAttribute(this.FirstName, newFirstName, "FirstName", registry);
+	}
+
+	public boolean changeLastName(String newLastName, Registry registry) {
+		return changeAttribute(this.LastName, newLastName, "LastName", registry);
+	}
+
+	public boolean changeUserPassword(String newUserPassword, Registry registry) {
+		return changeAttribute(this.UserPassword, newUserPassword, "UserPassword", registry);
+	}
+
+	public boolean changeUserName(String newUserName, Registry registry) {
+		return changeAttribute(this.UserName, newUserName, "UserName", registry);
+	}
+
+	public boolean changeUserEmail(String newUserEmail, Registry registry) {
+		return changeAttribute(this.UserEmail, newUserEmail, "UserEmail", registry);
+	}
+
+	public boolean changeUserPhoneNumber(String newUserPhoneNumber, Registry registry) {
+		return changeAttribute(this.PhoneNumber, newUserPhoneNumber, "PhoneNumber", registry);
+	}
 }
