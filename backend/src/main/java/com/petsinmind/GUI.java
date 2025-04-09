64ed6a7354ba0d7001a6c666f7d7e686ca98b854
ZioFalak <falak16sm@gmail.com>
2025-04-09 02:14:06 +0100

    gui changes

--- backend/src/main/java/com/petsinmind/GUI.java
+++ backend/src/main/java/com/petsinmind/GUI.java
@@ -1,9 +1,10 @@
 package com.petsinmind;
 
+import java.util.UUID;
 
-import com.petsinmind.users.User;
 import com.petsinmind.users.Caretaker;
 import com.petsinmind.users.PetOwner;
+import com.petsinmind.Application;
 
 public class GUI {
 
@@ -21,17 +22,28 @@ public class GUI {
             return false;
         }
         // Call the registry to authenticate the user
-        return registry.authenticateUser(username, password);
+        // return registry.authenticateUser(username, password);
+        return true;
     }
 
     // Method for user registration
-    public boolean registerPetOwner(String username, String password, String email, String phoneNumber, String firstName, String lastName) {
+    public boolean registerPetOwner(String username, String password, String email, String phoneNumber, String firstName, String lastName, String location) {
         // Check if the username and password are not empty
         if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
             return false;
         }
         // Call the registry to add a new user
-        PetOwner newUser = new PetOwner(username, password, email, phoneNumber, firstName, lastName);
+        PetOwner newUser = new PetOwner(UUID.randomUUID() ,username, password, email, phoneNumber, firstName, lastName, location, null, null, null, null);
         return registry.createUser(newUser);
+    }
+
+    public boolean registerCaretaker(String firstName, String lastName, String username, String password, String email, String phoneNumber) {
+        // Check if the username and password are not empty
+        if (username == null || username.isEmpty() || password == null || password.isEmpty()) {
+            return false;
+        }
+        // Create an application object for the caretaker
+        Application application = new Application(firstName, lastName, username, password, email, null, phoneNumber);
+        return registry.createApplication(application);
     }
 }
